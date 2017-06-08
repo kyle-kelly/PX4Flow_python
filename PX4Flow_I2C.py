5683115e7db9906f7988dc9a956aa3b3319eddec
kyle-kelly <kkelly.667@gmail.com>
2017-06-08 12:51:21 -0400

    A class and test for the PX4Flow optical sensor. Appears to be working.

---
+++ PX4Flow_I2C.py
@@ -0,0 +1,108 @@
+import smbus
+import time
+
+class PX4Flow_I2C(object):
+	"""
+	Class to hold I2C frame data from PX4Flow
+
+	typedef struct i2c_frame
+	{
+	    uint16_t frame_count;// counts created I2C frames [#frames]
+	    int16_t pixel_flow_x_sum;// latest x flow measurement in pixels*10 [pixels]
+	    int16_t pixel_flow_y_sum;// latest y flow measurement in pixels*10 [pixels]
+	    int16_t flow_comp_m_x;// x velocity*1000 [meters/sec]
+	    int16_t flow_comp_m_y;// y velocity*1000 [meters/sec]
+	    int16_t qual;// Optical flow quality / confidence [0: bad, 255: maximum quality]
+	    int16_t gyro_x_rate; // latest gyro x rate [rad/sec]
+	    int16_t gyro_y_rate; // latest gyro y rate [rad/sec]
+	    int16_t gyro_z_rate; // latest gyro z rate [rad/sec]
+	    uint8_t gyro_range; // gyro range [0 .. 7] equals [50 deg/sec .. 2000 deg/sec] 
+	    uint8_t sonar_timestamp;// time since last sonar update [milliseconds]
+	    int16_t ground_distance;// Ground distance in meters*1000 [meters]. Positive value: distance known. Negative value: Unknown distance
+	} i2c_frame;
+
+		typedef struct i2c_integral_frame
+	{
+	    uint16_t frame_count_since_last_readout;//number of flow measurements since last I2C readout [#frames]
+	    int16_t pixel_flow_x_integral;//accumulated flow in radians*10000 around x axis since last I2C readout [rad*10000]
+	    int16_t pixel_flow_y_integral;//accumulated flow in radians*10000 around y axis since last I2C readout [rad*10000]
+	    int16_t gyro_x_rate_integral;//accumulated gyro x rates in radians*10000 since last I2C readout [rad*10000] 
+	    int16_t gyro_y_rate_integral;//accumulated gyro y rates in radians*10000 since last I2C readout [rad*10000] 
+	    int16_t gyro_z_rate_integral;//accumulated gyro z rates in radians*10000 since last I2C readout [rad*10000] 
+	    uint32_t integration_timespan;//accumulation timespan in microseconds since last I2C readout [microseconds]
+	    uint32_t sonar_timestamp;// time since last sonar update [microseconds]
+	    int16_t ground_distance;// Ground distance in meters*1000 [meters*1000]
+	    int16_t gyro_temperature;// Temperature * 100 in centi-degrees Celsius [degcelsius*100]
+	    uint8_t quality;// averaged quality of accumulated flow values [0:bad quality;255: max quality]
+	} __attribute__((packed)) i2c_integral_frame;
+
+	"""
+	def __init__(self, bus, address):
+		self.name = "PX4Flow"
+		self.bus = bus
+		self.address = address
+
+		"""Initialize with negative values"""
+		#I2C frame values
+		self.frame_count = -1 #counts created I2C frames [#frames]
+		self.pixel_flow_x_sum = -1 #latest x flow measurement in pixels*10 [pixels]
+		self.pixel_flow_y_sum = -1 #latest y flow measurement in pixels*10 [pixels]
+		self.flow_comp_m_x = -1 #x velocity*1000 [meters/sec]
+		self.flow_comp_m_y = -1 #y velocity*1000 [meters/sec]
+		self.qual = -1 #Optical flow quality / confidence [0: bad, 255: maximum quality]
+		self.gyro_x_rate = -1 #latest gyro x rate [rad/sec]
+		self.gyro_y_rate = -1 #latest gyro y rate [rad/sec]
+		self.gyro_z_rate = -1 #latest gyro z rate [rad/sec]
+		self.gyro_range = -1 #gyro range [0 .. 7] equals [50 deg/sec .. 2000 deg/sec] 
+		self.sonar_timestamp = -1 #time since last sonar update [milliseconds]
+		self.ground_distance = -1 #Ground distance in meters*1000 [meters]. Positive value: distance known. Negative value: Unknown distance
+
+	    #Integral I2C frame values
+		self.frame_count_since_last_readout = -1 #number of flow measurements since last I2C readout [#frames]
+		self.pixel_flow_x_integral = -1 #accumulated flow in radians*10000 around x axis since last I2C readout [rad*10000]
+		self.pixel_flow_y_integral = -1 #accumulated flow in radians*10000 around y axis since last I2C readout [rad*10000]
+		self.gyro_x_rate_integral = -1 #accumulated gyro x rates in radians*10000 since last I2C readout [rad*10000] 
+		self.gyro_y_rate_integral = -1 #accumulated gyro y rates in radians*10000 since last I2C readout [rad*10000] 
+		self.gyro_z_rate_integral = -1 #accumulated gyro z rates in radians*10000 since last I2C readout [rad*10000] 
+		self.integration_timespan = -1 #accumulation timespan in microseconds since last I2C readout [microseconds]
+		self.sonar_timestamp = -1 # time since last sonar update [microseconds]
+		self.ground_distance = -1 # Ground distance in meters*1000 [meters*1000]
+		self.gyro_temperature = -1 # Temperature * 100 in centi-degrees Celsius [degcelsius*100]
+		self.quality = -1 # averaged quality of accumulated flow values [0:bad quality;255: max quality]
+
+	def update(self):
+		"""Send 0x0 to PX4FLOW module and receive back 22 bytes of data in registers 0x00-0x15"""
+
+		self.bus.write_byte(self.address, 0x0)
+		i2c_frame = self.bus.read_i2c_block_data(self.address, 0x00, 22)
+
+		self.frame_count = i2c_frame[0] | (i2c_frame[1] << 8)
+		self.pixel_flow_x_sum = i2c_frame[2] | (i2c_frame[3] << 8)
+		self.pixel_flow_y_sum = i2c_frame[4] | (i2c_frame[5] << 8)
+		self.flow_comp_m_x = i2c_frame[6] | (i2c_frame[7] << 8)
+		self.flow_comp_m_y = i2c_frame[8] | (i2c_frame[9] << 8)
+		self.qual = i2c_frame[10] | (i2c_frame[11] << 8)
+		self.gyro_x_rate = i2c_frame[12] | (i2c_frame[13] << 8)
+		self.gyro_y_rate = i2c_frame[14] | (i2c_frame[15] << 8)
+		self.gyro_z_rate = i2c_frame[16] | (i2c_frame[17] << 8)
+		self.gyro_range = i2c_frame[18]
+		self.sonar_timestamp = i2c_frame[19]
+		self.ground_distance = i2c_frame[20] | (i2c_frame[21] << 8)
+
+	def integral_update(self):
+		"""Send 0x16 to PX4FLOW module and receive back 25 bytes of data in registers 0x16-0x2E"""
+
+		self.bus.write_byte(self.address, 0x16)
+		i2c_integral_frame = self.bus.read_i2c_block_data(self.address, 0x16, 25)
+
+		self.frame_count_since_last_readout = i2c_integral_frame[0] | (i2c_integral_frame[1] << 8)
+		self.pixel_flow_x_integral = i2c_integral_frame[2] | (i2c_integral_frame[3] << 8)
+		self.pixel_flow_y_integral = i2c_integral_frame[4] | (i2c_integral_frame[5] << 8)
+		self.gyro_x_rate_integral = i2c_integral_frame[6] | (i2c_integral_frame[7] << 8)
+		self.gyro_y_rate_integral = i2c_integral_frame[8] | (i2c_integral_frame[9] << 8)
+		self.gyro_z_rate_integral = i2c_integral_frame[10] | (i2c_integral_frame[11] << 8)
+		self.integration_timespan = i2c_integral_frame[12] | (i2c_integral_frame[13] << 8)
+		self.sonar_timestamp = i2c_integral_frame[14] | (i2c_integral_frame[15] << 8) | (i2c_integral_frame[16] << 16) | (i2c_integral_frame[17] << 24)
+		self.ground_distance = i2c_integral_frame[18] | (i2c_integral_frame[19] << 8) | (i2c_integral_frame[20] << 16) | (i2c_integral_frame[21] << 24)
+		self.gyro_temperature = i2c_integral_frame[22] | (i2c_integral_frame[23] << 8)
+		self.quality = i2c_integral_frame[24]
---
+++ PX4Flow_test.py
@@ -0,0 +1,160 @@
+"""A script to communicate with PX4Flow from the command line"""
+
+import PX4Flow_I2C as px4f
+import smbus
+import time
+import textwrap
+
+def help():
+	print textwrap.dedent("""\
+            COMMANDS
+            'update': update the I2C frame and print related values
+            'integral_update': update the integral I2C frame and print related values
+            'quit': quit program
+            'help': this menu
+            """)
+
+def main():
+	"""Initilize I2C parameters and checks for user input"""
+
+	#On I2C bus 1
+	bus = smbus.SMBus(1)
+	#Default address from docs (hardware assigned)
+	address = 0x42
+
+	#Create sensor instance
+	px4flow = px4f.PX4Flow_I2C(bus, address)
+
+	print "Initial values:\n"
+	#I2C frame values
+	print px4flow.frame_count #counts created I2C frames [#frames]
+	print px4flow.pixel_flow_x_sum #latest x flow measurement in pixels*10 [pixels]
+	print px4flow.pixel_flow_y_sum #latest y flow measurement in pixels*10 [pixels]
+	print px4flow.flow_comp_m_x #x velocity*1000 [meters/sec]
+	print px4flow.flow_comp_m_y #y velocity*1000 [meters/sec]
+	print px4flow.qual #Optical flow quality / confidence [0: bad, 255: maximum quality]
+	print px4flow.gyro_x_rate #latest gyro x rate [rad/sec]
+	print px4flow.gyro_y_rate #latest gyro y rate [rad/sec]
+	print px4flow.gyro_z_rate #latest gyro z rate [rad/sec]
+	print px4flow.gyro_range #gyro range [0 .. 7] equals [50 deg/sec .. 2000 deg/sec] 
+	print px4flow.sonar_timestamp #time since last sonar update [milliseconds]
+	print px4flow.ground_distance #Ground distance in meters*1000 [meters]. Positive value: distance known. Negative value: Unknown distance
+
+	#Integral I2C frame values
+	print px4flow.frame_count_since_last_readout #number of flow measurements since last I2C readout [#frames]
+	print px4flow.pixel_flow_x_integral #accumulated flow in radians*10000 around x axis since last I2C readout [rad*10000]
+	print px4flow.pixel_flow_y_integral #accumulated flow in radians*10000 around y axis since last I2C readout [rad*10000]
+	print px4flow.gyro_x_rate_integral #accumulated gyro x rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.gyro_y_rate_integral #accumulated gyro y rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.gyro_z_rate_integral #accumulated gyro z rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.integration_timespan #accumulation timespan in microseconds since last I2C readout [microseconds]
+	print px4flow.sonar_timestamp # time since last sonar update [microseconds]
+	print px4flow.ground_distance # Ground distance in meters*1000 [meters*1000]
+	print px4flow.gyro_temperature # Temperature * 100 in centi-degrees Celsius [degcelsius*100]
+	print px4flow.quality # averaged quality of accumulated flow values [0:bad quality;255: max quality]
+	
+	print "Update I2C Frame:\n"
+	px4flow.update()
+	#I2C frame values
+	print px4flow.frame_count #counts created I2C frames [#frames]
+	print px4flow.pixel_flow_x_sum #latest x flow measurement in pixels*10 [pixels]
+	print px4flow.pixel_flow_y_sum #latest y flow measurement in pixels*10 [pixels]
+	print px4flow.flow_comp_m_x #x velocity*1000 [meters/sec]
+	print px4flow.flow_comp_m_y #y velocity*1000 [meters/sec]
+	print px4flow.qual #Optical flow quality / confidence [0: bad, 255: maximum quality]
+	print px4flow.gyro_x_rate #latest gyro x rate [rad/sec]
+	print px4flow.gyro_y_rate #latest gyro y rate [rad/sec]
+	print px4flow.gyro_z_rate #latest gyro z rate [rad/sec]
+	print px4flow.gyro_range #gyro range [0 .. 7] equals [50 deg/sec .. 2000 deg/sec] 
+	print px4flow.sonar_timestamp #time since last sonar update [milliseconds]
+	print px4flow.ground_distance #Ground distance in meters*1000 [meters]. Positive value: distance known. Negative value: Unknown distance
+
+	#Integral I2C frame values
+	print px4flow.frame_count_since_last_readout #number of flow measurements since last I2C readout [#frames]
+	print px4flow.pixel_flow_x_integral #accumulated flow in radians*10000 around x axis since last I2C readout [rad*10000]
+	print px4flow.pixel_flow_y_integral #accumulated flow in radians*10000 around y axis since last I2C readout [rad*10000]
+	print px4flow.gyro_x_rate_integral #accumulated gyro x rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.gyro_y_rate_integral #accumulated gyro y rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.gyro_z_rate_integral #accumulated gyro z rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.integration_timespan #accumulation timespan in microseconds since last I2C readout [microseconds]
+	print px4flow.sonar_timestamp # time since last sonar update [microseconds]
+	print px4flow.ground_distance # Ground distance in meters*1000 [meters*1000]
+	print px4flow.gyro_temperature # Temperature * 100 in centi-degrees Celsius [degcelsius*100]
+	print px4flow.quality # averaged quality of accumulated flow values [0:bad quality;255: max quality]
+
+	print "Update I2C Integral Frame:\n"
+	px4flow.integral_update()
+	#I2C frame values
+	print px4flow.frame_count #counts created I2C frames [#frames]
+	print px4flow.pixel_flow_x_sum #latest x flow measurement in pixels*10 [pixels]
+	print px4flow.pixel_flow_y_sum #latest y flow measurement in pixels*10 [pixels]
+	print px4flow.flow_comp_m_x #x velocity*1000 [meters/sec]
+	print px4flow.flow_comp_m_y #y velocity*1000 [meters/sec]
+	print px4flow.qual #Optical flow quality / confidence [0: bad, 255: maximum quality]
+	print px4flow.gyro_x_rate #latest gyro x rate [rad/sec]
+	print px4flow.gyro_y_rate #latest gyro y rate [rad/sec]
+	print px4flow.gyro_z_rate #latest gyro z rate [rad/sec]
+	print px4flow.gyro_range #gyro range [0 .. 7] equals [50 deg/sec .. 2000 deg/sec] 
+	print px4flow.sonar_timestamp #time since last sonar update [milliseconds]
+	print px4flow.ground_distance #Ground distance in meters*1000 [meters]. Positive value: distance known. Negative value: Unknown distance
+
+	#Integral I2C frame values
+	print px4flow.frame_count_since_last_readout #number of flow measurements since last I2C readout [#frames]
+	print px4flow.pixel_flow_x_integral #accumulated flow in radians*10000 around x axis since last I2C readout [rad*10000]
+	print px4flow.pixel_flow_y_integral #accumulated flow in radians*10000 around y axis since last I2C readout [rad*10000]
+	print px4flow.gyro_x_rate_integral #accumulated gyro x rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.gyro_y_rate_integral #accumulated gyro y rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.gyro_z_rate_integral #accumulated gyro z rates in radians*10000 since last I2C readout [rad*10000] 
+	print px4flow.integration_timespan #accumulation timespan in microseconds since last I2C readout [microseconds]
+	print px4flow.sonar_timestamp # time since last sonar update [microseconds]
+	print px4flow.ground_distance # Ground distance in meters*1000 [meters*1000]
+	print px4flow.gyro_temperature # Temperature * 100 in centi-degrees Celsius [degcelsius*100]
+	print px4flow.quality # averaged quality of accumulated flow values [0:bad quality;255: max quality]
+
+	while True:
+		
+		user_func = raw_input("-->")
+
+		if user_func == 'update':
+			px4flow.update()
+			#I2C frame values
+			print px4flow.frame_count #counts created I2C frames [#frames]
+			print px4flow.pixel_flow_x_sum #latest x flow measurement in pixels*10 [pixels]
+			print px4flow.pixel_flow_y_sum #latest y flow measurement in pixels*10 [pixels]
+			print px4flow.flow_comp_m_x #x velocity*1000 [meters/sec]
+			print px4flow.flow_comp_m_y #y velocity*1000 [meters/sec]
+			print px4flow.qual #Optical flow quality / confidence [0: bad, 255: maximum quality]
+			print px4flow.gyro_x_rate #latest gyro x rate [rad/sec]
+			print px4flow.gyro_y_rate #latest gyro y rate [rad/sec]
+			print px4flow.gyro_z_rate #latest gyro z rate [rad/sec]
+			print px4flow.gyro_range #gyro range [0 .. 7] equals [50 deg/sec .. 2000 deg/sec] 
+			print px4flow.sonar_timestamp #time since last sonar update [milliseconds]
+			print px4flow.ground_distance #Ground distance in meters*1000 [meters]. Positive value: distance known. Negative value: Unknown distance
+
+		elif user_func == 'integral_update':
+			px4flow.integral_update()
+			#Integral I2C frame values
+			print px4flow.frame_count_since_last_readout #number of flow measurements since last I2C readout [#frames]
+			print px4flow.pixel_flow_x_integral #accumulated flow in radians*10000 around x axis since last I2C readout [rad*10000]
+			print px4flow.pixel_flow_y_integral #accumulated flow in radians*10000 around y axis since last I2C readout [rad*10000]
+			print px4flow.gyro_x_rate_integral #accumulated gyro x rates in radians*10000 since last I2C readout [rad*10000] 
+			print px4flow.gyro_y_rate_integral #accumulated gyro y rates in radians*10000 since last I2C readout [rad*10000] 
+			print px4flow.gyro_z_rate_integral #accumulated gyro z rates in radians*10000 since last I2C readout [rad*10000] 
+			print px4flow.integration_timespan #accumulation timespan in microseconds since last I2C readout [microseconds]
+			print px4flow.sonar_timestamp # time since last sonar update [microseconds]
+			print px4flow.ground_distance # Ground distance in meters*1000 [meters*1000]
+			print px4flow.gyro_temperature # Temperature * 100 in centi-degrees Celsius [degcelsius*100]
+			print px4flow.quality # averaged quality of accumulated flow values [0:bad quality;255: max quality]
+		
+		elif user_func == 'help':
+			help()
+
+		elif user_func == 'quit':
+			quit()
+		
+		else:
+			print "Invalid input. Type 'help' for a list of commands.\n"
+
+
+if __name__ == "__main__":
+    main()
